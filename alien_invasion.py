6871c37a49ace60d5d8f70b6908d7403d529ad9a
Vy Hong <contact@vyhong.me>
2018-01-05 22:00:54 -0800

    Refactor alien_invasion extract methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,22 @@ from settings import Settings
 from ship import Ship
 
 
+def check_events():
+    '''Watch for keyboard and mouse events.'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def draw_screen(settings, screen, ship):
+    '''Update images on the screen and flip to the new screen.'''
+    screen.fill(settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently draw screen visible
+    pygame.display.flip()
+
+
 def run_game():
     pygame.init()
     pygame.display.set_caption('Alien Invasion')
@@ -15,16 +31,8 @@ def run_game():
     ship = Ship(screen)
 
     while True:
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently draw screen visible
-        pygame.display.flip()
+        check_events()
+        draw_screen(settings, screen, ship)
 
 
 if __name__ == '__main__':
