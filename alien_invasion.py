eca215a4835abcabdf162519e120004e35bb6058
Vy Hong <contact@vyhong.me>
2018-01-05 21:37:02 -0800

    Add basic game screen

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+
+import pygame
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('Alien Invasion')
+
+    bg_color = (230, 230, 230)
+
+    while True:
+        # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+
+        # Make the most recently draw screen visible
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    run_game()
